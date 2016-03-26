06cc5a9376f2d009755fac80bd958f1aaec4f819
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-03-27 05:56:45 +0900

    add minimum mruby include file

--- src/mrb_opentoonz_core.cpp
+++ src/mrb_opentoonz_core.cpp
@@ -1,9 +1,16 @@
 #include <stdio.h>
+
 #include <toonz_plugin.h>
 #include <toonz_hostif.h>
 
 #include <utils/rect.hpp>
 #include <utils/interf_holder.hpp>
+
+#include <mruby.h>
+#include <mruby/compile.h>
+#include <mruby/object.h>
+#include <mruby/class.h>
+#include <mruby/string.h>
 
 
 toonz::host_interface_t *ifactory_ = NULL;
